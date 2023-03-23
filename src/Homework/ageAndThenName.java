a5ffdfd314baf6cf00ad93ba20b8bc894d3f4f90
KS1502 <kirakrg@mail.ru>
2023-03-23 09:50:14 +0100

    Homework Employee

--- src/Homework/ageAndThenName.java
+++ src/Homework/ageAndThenName.java
@@ -6,7 +6,11 @@ public class ageAndThenName implements Comparator<Employee> {
 
     @Override
     public int compare(Employee o1, Employee o2) {
-        return 0;
+        if(o2.getAge() == o1.getAge()){
+            return o1.getName().compareTo(o2.getName());
+        }
+        return o1.getAge()-o2.getAge();
+
 
     }
 }
